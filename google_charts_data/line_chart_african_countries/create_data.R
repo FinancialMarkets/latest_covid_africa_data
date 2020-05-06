cf8adbc658d9645a9ce8eeb05e19b911eb46be7e
Matt Brigida <mdbrigida@gmail.com>
2020-05-06 13:13:00 -0400

    africa data google charts

---
+++ google_charts_data/line_chart_african_countries/create_data.R
@@ -0,0 +1,38 @@
+library(tidyverse)
+library(reshape2)
+
+alldata <- read_csv("../../african_latest_data.csv")
+
+## cumulative cases and deaths per million---------
+
+alldata$cum_cases_per_million <- round(alldata$cumulative_cases / alldata$popData2018, 3)
+alldata$cum_deaths_per_million <- round(alldata$cumulative_deaths / alldata$popData2018, 3)
+
+## for line chart, convert into days on file and cumulative cases/deaths by country in columns-----------
+
+alldata$const <- 1
+
+linedata <- alldata %>%
+  group_by(countryterritoryCode) %>%
+  arrange(dateRep) %>%
+  mutate(days_data = cumsum(const)) 
+
+linedata_total_cases <- linedata[, c("days_data", "countriesAndTerritories", "cumulative_cases")]
+linedata_total_deaths <- linedata[, c("days_data", "countriesAndTerritories", "cumulative_deaths")]
+
+linedata_cases_per_million <- linedata[, c("days_data", "countriesAndTerritories", "cum_cases_per_million")]
+linedata_deaths_per_million <- linedata[, c("days_data", "countriesAndTerritories", "cum_deaths_per_million")]
+
+## reshape so country names are columns----------------------
+cases_data_to_gchart <- dcast(linedata_total_cases, days_data ~ countriesAndTerritories)
+deaths_data_to_gchart <- dcast(linedata_total_deaths, days_data ~ countriesAndTerritories)
+
+##change NA to null
+cases_data_to_gchart[is.na(cases_data_to_gchart)] <- "null"
+deaths_data_to_gchart[is.na(deaths_data_to_gchart)] <- "null"
+
+
+write_csv(cases_data_to_gchart, "line_chart_cases.csv")
+write_csv(deaths_data_to_gchart, "line_chart_deaths.csv")
+
+
